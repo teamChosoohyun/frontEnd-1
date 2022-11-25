0e9136f31c1e376d48631cdca917339de96e52ef
jeongdonggyeom <topcome928@gmail.com>
2022-11-25 20:57:53 +0900

    이미지 태그 수정

--- pages/main/lecturer.js
+++ pages/main/lecturer.js
@@ -6,31 +6,31 @@ export default function Lecturer(){
         <div className={styles.lecturer}>
             <div className={`${styles.lec} ${styles.one}`}>
                 <span className={styles.lecImg}>
-                    <Image src={"/images/lecturer.png"} alt={"icon"} width={100} height={100} layout={"fill"} />
+                    <Image src={"/images/lecturer.png"} alt={"icon"} layout={"fill"} />
                 </span>
                 <p className={styles.lecField}>소프트웨어</p>
             </div>
             <div className={`${styles.lec} ${styles.two}`}>
                 <span className={styles.lecImg}>
-                    <Image src={"/images/lecturer2.png"} alt={"icon"} width={100} height={100} layout={"fill"} />
+                    <Image src={"/images/lecturer2.png"} alt={"icon"} layout={"fill"} />
                 </span>
                 <p className={styles.lecField}>레고</p>
             </div>
             <div className={`${styles.lec} ${styles.three}`}>
                 <span className={styles.lecImg}>
-                    <Image src={"/images/lecturer3.png"} alt={"icon"} width={100} height={100} layout={"fill"} />
+                    <Image src={"/images/lecturer3.png"} alt={"icon"} layout={"fill"} />
                 </span>
                 <p className={styles.lecField}>메이커</p>
             </div>
             <div className={`${styles.lec} ${styles.four}`}>
                 <span className={styles.lecImg}>
-                    <Image src={"/images/lecturer4.png"} alt={"icon"} width={100} height={100} layout={"fill"} />
+                    <Image src={"/images/lecturer4.png"} alt={"icon"} layout={"fill"} />
                 </span>
                 <p className={styles.lecField}>레고</p>
             </div>
             <div className={`${styles.lec} ${styles.five}`}>
                 <span className={styles.lecImg}>
-                    <Image src={"/images/lecturer5.png"} alt={"icon"} width={100} height={100} layout={"fill"} />
+                    <Image src={"/images/lecturer5.png"} alt={"icon"} layout={"fill"} />
                 </span>
                 <p className={styles.lecField}>소프트웨어</p>
             </div>
