0ce42fd1ff3ee7382882b897620deab63c23b3b3
parkdaxun <parkde0207@gmail.com>
2022-12-20 09:31:41 +0900

    Contents pages

--- pages/contents/all.js
+++ pages/contents/all.js
@@ -9,10 +9,10 @@ export default function All() {
         <div className={styles.root}>
             <div className={styles.class}>
                 <Fade bottom>
-                <h2>레고 교육</h2>
+                <h2>레고 공학 교육</h2>
                 <div className={styles.explain}>
                     <Image src={Lego_pic}/>
-                    <p>레고수업에 대한 내용(추후 수정)</p>
+                    <p>자신만의 로봇을 만들어 보는 과정을 통해 공학적 사고와 문제해결력을 키울 수 있습니다.</p>
                 </div>
                 </Fade>
             </div>
@@ -21,7 +21,7 @@ export default function All() {
                 <h2>소프트웨어 교육</h2>
                 <div className={styles.explain}>
                     <Image src={Software_pic}/>
-                    <p>소프트웨어 교육에 대한 내용(추후 수정)</p>
+                    <p>사물인터넷, 앱 개발, 인공지능에 이르기까지 다양한 소프트웨어교육을 통해 편리한 세상을 만들어 봅시다.</p>
                 </div>
                 </Fade>
             </div>
@@ -30,7 +30,7 @@ export default function All() {
                 <h2>메이커 교육</h2>
                 <div className={styles.explain}>
                     <Image src={Maker_pic}/>
-                    <p>메이커 교육에 대한 내용(추후 수정)</p>
+                    <p>생활에 필요한 물건들을 창의적으로 만들어보는 메이커교육은 여러분의 다양한 능력을 발현시켜 줄 것입니다.</p>
                 </div>
                 </Fade>
             </div>
--- pages/contents/lego.js
+++ pages/contents/lego.js
@@ -7,14 +7,15 @@ export default function Lego() {
         <div>
             <Fade bottom>
             <div className={styles.title}>
-                <p>레고 교육</p>
+                <p>레고 공학 교육</p>
             </div>
             <div className={styles.lego_img}><Image src={Lego_pic}/></div>
             <div className={`${styles.title} ${styles.top2}`}>
-                <p>레고 교육이란?</p>
+                <p>레고 공학 교육이란?</p>
             </div>
             <div className={styles.explain}>
-                <p>레고 교육이란 레고 교육 (추후 수정 예정)</p>
+                <p>레고 블록은 언제 어디서든 쉽게 만들고 수정할 수 있으며, 분해할 수 있어 자신이 상상한 아이디어를 손쉽게 구현해 볼 수 있습니다.</p>
+                <p>특히, 코딩을 통해 움직이는 레고를 만들어 자신만의 로봇을 만들어 보는 과정을 통해 공학적 사고와 문제해결력을 키울 수 있습니다.</p>
             </div>
             </Fade>
         </div>
